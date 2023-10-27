4dcc4aae22de255d9e163e50ea4ae8fd4d1a6f26
Sofiane <sofianemahdi89@gmail.com>
2023-10-27 17:33:11 +0100

    upload

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+$name = $_post['name'];
+$visitor_email = $_post['email'];
+$subject = $_post['subject'];
+$message = $_post['message'];
+
+$email_form = 'info@Eduford.com'
+$email_subject = 'New form submission'
+$email_body = "User Name :$name.\n".
+							"User email :$visitor_email.\n".
+							"Subject :$subject.\n".
+							"User message :$message.\n";
+$to = 'sofianemahdi89@gmail.com';
+$headers = "from: $email_form \r\n";
+$headers .= "Reply_to: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html")
+?>
